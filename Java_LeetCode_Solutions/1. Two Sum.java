92553f91591a7e2b6f97c149044d62a0e0a01d84
asmi325 <asmibhawsar18@gmail.com>
2025-01-11 13:47:26 +0530

    Update 1. Two Sum.java

--- Java_LeetCode_Solutions/1. Two Sum.java
+++ Java_LeetCode_Solutions/1. Two Sum.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j;
+        int[] ans= new int[2];
+        int start=o;
+        int end= nums.length-1;
 
+        while(start<end){
+            int sum= nums[start]+nums[end];
+            if(sum==target){
+                ans[0]=start;
+                ans[1]=end;
+            }
+            else if(sum>target){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+        
+    }
+    return ans;
+}
