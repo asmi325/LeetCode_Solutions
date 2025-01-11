97a84491772f7a6283a5931a862fcd6df5bae8dd
asmi325 <asmibhawsar18@gmail.com>
2025-01-11 19:10:51 +0530

    Update 1. Two Sum.py

--- Python_LeetCode_Solutions/1. Two Sum.py
+++ Python_LeetCode_Solutions/1. Two Sum.py
@@ -1,1 +1,11 @@
-shillu
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            # Check if the difference is already in the dictionary
+            if diff in num_dict:
+                return [num_dict[diff], i]
+            # Store the number with its index
+            num_dict[num] = i
+        return []
